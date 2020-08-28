dc791050a27ef6654ffaf878ae0c36a1ea354ff8
danivelop <55433950+danivelop@users.noreply.github.com>
2020-08-28 23:09:16 +0900

    서버 도메인 추가 (#72)
    * chore: #70. 서버 도메인 변경에 따른 proxy재설정
    
    * 빠뜨렸던 서버 도메인 설정 추가

--- src/utils/requestUtils.ts
+++ src/utils/requestUtils.ts
@@ -4,7 +4,7 @@ import { getEnvironment } from 'utils/environmentUtils'
 export function getRequestUrl() {
   switch (getEnvironment()) {
     case 'production':
-      return 'http://ec2-15-164-188-237.ap-northeast-2.compute.amazonaws.com:8080'
+      return 'http://ec2-15-165-190-104.ap-northeast-2.compute.amazonaws.com'
     default:
       return ''
   }
