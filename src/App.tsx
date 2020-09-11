c9989666f2eadaf6fdf1cc82840337c70c1ea5a3
danivelop <55433950+danivelop@users.noreply.github.com>
2020-09-12 03:12:19 +0900

    feat: 채널톡 플러그인 추가 (#101)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* External dependencies */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 /* Internal dependencies */
@@ -7,10 +7,17 @@ import Helmet from 'components/Helmet'
 import { HomePage, ErrorPage, InvitationPage, PreviewPage } from 'pages'
 import ErrorHandler from 'components/ErrorHandler'
 import ScrollToTop from 'components/ScrollToTop'
+import ChannelService from 'services/ChannelService'
 import { GlobalStyle } from 'styles/global-styles'
 import 'app.scss'
 
 function App() {
+  useEffect(() => {
+    ChannelService.boot({
+      pluginKey: '2146f61e-2956-413b-bdda-c9e997fdaad8',
+    })
+  }, [])
+
   return (
     <BrowserRouter>
       <Helmet title="나와 초대장" />
