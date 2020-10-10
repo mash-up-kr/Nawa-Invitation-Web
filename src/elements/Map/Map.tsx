acb4d775af3e83376978b2227a8273e763be6a38
danivelop <55433950+danivelop@users.noreply.github.com>
2020-10-10 12:33:10 +0900

    feat: #111. 지도에서 스크롤막기 (#112)

--- src/elements/Map/Map.tsx
+++ src/elements/Map/Map.tsx
@@ -22,6 +22,10 @@ function Map({ map, placeName }: MapProps) {
     setIsWideMap(prev => !prev)
   }, [])
 
+  const handleScrollMap = useCallback(e => {
+    e.stopPropagation()
+  }, [])
+
   useEffect(() => {
     mapService.current = new KakaoMapService(mapContainer.current, map.latitude, map.longitude)
     mapService.current.loadMap()
@@ -45,7 +49,7 @@ function Map({ map, placeName }: MapProps) {
           {map.roadAddress}
         </Styled.RoadAddress>
       </Styled.AddressWrapper>
-      <Styled.MapContainer ref={mapContainer} isWideMap={isWideMap} />
+      <Styled.MapContainer ref={mapContainer} isWideMap={isWideMap} onWheel={handleScrollMap} />
       <Styled.MapSizeIconWrapper onClick={handleClickMapSize}>
         <Styled.MapSizeIcon name={isWideMap ? 'squares-filled' : 'squares'} size={Size.Small} />
       </Styled.MapSizeIconWrapper>
